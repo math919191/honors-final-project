1e1358959c5087352e0f372765b2d416bc3f479a
Gabriel <gabriellizam@gmail.com>
2023-04-13 12:45:15 -0600

    test

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,7 +2,7 @@ export function HomePage(props){
 
     return (
         <div>
-            <h1 style="color:blue;">Home Page</h1>
+            <h1>Home Page</h1>
             <p>Some paragraph description about our page</p>
             <button onClick={() => props.nextPageFunction()}>Click here to take the quiz</button>
         </div>
